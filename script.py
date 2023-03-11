feba12e3830a499544689f93b58bd083e643b114
ArdonitSaliji <ardonit.1980@gmail.com>
2023-03-11 13:29:22 +0100

    some comments added

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ def push_to_github(repo_path, file_path, start_date):
     os.chdir(repo_path)
     os.system('git add .')
 
-    for i in range(5):
+    for i in range(5): # How many days to add squares 5 will add 5 green squares
         date = start_date + datetime.timedelta(days=i)
         date_string = date.strftime("%Y-%m-%d")
         message = f'Added message #{i+1} on {date_string}'
@@ -24,8 +24,8 @@ def push_to_github(repo_path, file_path, start_date):
 
 if __name__ == '__main__':
     file_path = './file.txt'
-    repo_path = '/home/ardonit/Desktop/Github/c-lang'
-    start_date = datetime.datetime(2022, 3, 4)
+    repo_path = '/home/ardonit/Desktop/Github/c-lang' # Your repo dir to add the commits
+    start_date = datetime.datetime(2022, 3, 4) # Edit this line from where to start adding green squares
     push_to_github(repo_path, file_path, start_date)
 
 
